5eb8da0c8813f8e19c00336aaff1415d415ff700
Alexandr Koksharov <alexandr.koksharov@oracle.com>
2019-01-17 13:13:41 +0100

    Initial Commit

---
+++ StorageClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StorageClient
+//
+//  Created by Koksharov Alexandr on 17/01/2019.
+//  Copyright © 2019 Koksharov Alexandr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
